59bfa11db6681db3b9442103be393041cf222ff3
tester <thomaslam2142>
2025-02-26 17:06:48 -0500

    Update gitignore and install stuff

--- install_packages.sh
+++ install_packages.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Install tkinter system package
+echo "Installing python3-tk..."
+sudo apt install -y python3-tk
+
+# Install Python packages from requirements.txt
+echo "Installing packages from requirements.txt..."
 while read package; do
     pip install "$package"
 done < requirements.txt
